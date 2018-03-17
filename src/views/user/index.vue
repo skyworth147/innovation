6b6dfee466666f8ce8b181c92682a8cdbbc350a9
sheepig <2270746205@qq.com>
2018-03-17 10:16:20 +0800

    components of user center

---
+++ src/views/user/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div id = 'side-list'>
+      <h1>个人中心</h1>
+      <ul>
+        <router-link v-for = 'item in nav' v-bind:to = 'item.path' tag = 'li' exact=" true"> {{ item.title }}</router-link>
+      </ul>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "index",
+      data() {
+        return {
+          nav: [
+            {
+              title: '账号管理',
+              path: '/user/management'
+            }, {
+              title: '个人简历',
+              path: '/user/resume'
+            }, {
+              title: '消息',
+              path: '/user/message',
+            }, {
+              title: '订阅',
+              path: '/user/book'
+            }
+          ]
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/user/management.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>management</p>
+  </div>
+
+</template>
+
+<script>
+    export default {
+      name: "management",
+      data() {
+        return {
+          info: {
+            avator: '',
+            name: '',
+            gender: '',
+            province: '',
+            city: '',
+            school: '',
+
+
+          }
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
